8dbcf2bee3324425c141ff8346365462ab718e38
Quan Nguyen <quanlh3052004@gmail.com>
2023-05-10 21:32:13 +0700

    update

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -1,112 +1,81 @@
 #include "painter.h"
+#include <random>
+#include <chrono>
+using namespace std;
 
-/***
-    Args: color (SDL_Color): color value 
-        
-    Returns:
-        None
-***/
-void Painter::setColor(SDL_Color color) 
-{ 
-    // TODO: set the color value for the Painter and set Render Draw Color
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int random(int l, int r)
+{
+    return rng() % (r - l + 1) + l;
 }
-
-
-/***
-    Args: numPixel (int): number of pixel for jumping forward
-        
-    Returns:
-        None
-***/
+void Painter::setColor(SDL_Color color)
+{
+    // TODO: set the color value for the Painter and set Render Draw Color
+    this->color = color;
+    SDL_SetRenderDrawColor(this->renderer, color.r, color.g, color.b, color.a);
+}
 void Painter::jumpForward(int numPixel)
 {
     // TODO: jump the painter forward
+    this->x += numPixel * cos(angle / 180 * M_PI);
+    this->y -= numPixel * sin(angle / 180 * M_PI);
 }
-
-
-/***
-    Args: numPixel (int): number of pixel for jumping backward
-        
-    Returns:
-        None
-***/
 void Painter::jumpBackward(int numPixel)
 {
     // TODO: jump the painter backward
+    this->x -= numPixel * cos(angle / 180 * M_PI);
+    this->y += numPixel * sin(angle / 180 * M_PI);
 }
-
-
-/***
-    Args: degree (double): the value of rotation angle
-        
-    Returns:
-        None
-***/       
 void Painter::turnLeft(double degree)
-{
-    // TODO: rotate left the painter   
+{// TODO: rotate left the painter
+    angle += degree;
+    angle = fmod(this->angle, 360);
 }
-
-
-/***
-    Args: degree (double): the value of rotation angle
-        
-    Returns:
-        None
-***/     
 void Painter::turnRight(double degree)
 {
     // TODO: rotate right the painter   
+    // TODO: rotate right the painter
+    this->angle -= degree;
+    angle = fmod(this->angle, 360);
 }
-
-/***  
-    Args: 
-        None
-    Returns:
-        None
-***/
 void Painter::randomColor()
 {
     // TODO: set random color    
+    // TODO: set random color
+    vector<SDL_Color> colorList = {CYAN_COLOR, BLUE_COLOR, ORANGE_COLOR, YELLOW_COLOR, LIME_COLOR, PURPLE_COLOR, RED_COLOR, WHITE_COLOR, BLACK_COLOR, GREEN_COLOR};
+    this->color = colorList[random(0, (int)colorList.size() - 1)];
 }
-
-
-/***
-Part of code that not need to be implemented
-***/
 void Painter::clearWithBgColor(SDL_Color bgColor)
 {
     SDL_Color curColor = color;
-    setColor(bgColor);
-	SDL_RenderClear(renderer);    
+    setColor(bgColor);SDL_RenderClear(renderer);
     setColor(curColor);
 }
-
-
-Painter::Painter(SDL_Window* window, SDL_Renderer *renderer)
+Painter::Painter(SDL_Window *window, SDL_Renderer *renderer)
 {
     SDL_RenderGetLogicalSize(renderer, &width, &height);
-    if (width == 0 && height == 0) {
+    if (width == 0 && height == 0)
+    {
         SDL_GetWindowSize(window, &width, &height);
     }
     this->renderer = renderer;
-    setPosition(width/2, height/2);
+    setPosition(width / 2, height / 2);
     setAngle(0);
     setColor(WHITE_COLOR);
     clearWithBgColor(BLUE_COLOR);
 }
-
-
 void Painter::createCircle(int radius)
 {
     double rad = (angle / 180) * M_PI;
-    int centerX = x + (int) (cos(rad) * (double) radius);;
-    int centerY = y - (int) (sin(rad) * (double) radius);;
+    int centerX = x + (int)(cos(rad) * (double)radius);
+    ;
+    int centerY = y - (int)(sin(rad) * (double)radius);
+    ;
 
     int dx = radius;
     int dy = 0;
-    int err = 0;
-
+int err = 0;
     while (dx >= dy)
     {
         SDL_RenderDrawPoint(renderer, centerX + dx, centerY + dy);
@@ -117,53 +86,40 @@ void Painter::createCircle(int radius)
         SDL_RenderDrawPoint(renderer, centerX - dy, centerY - dx);
         SDL_RenderDrawPoint(renderer, centerX + dy, centerY - dx);
         SDL_RenderDrawPoint(renderer, centerX + dx, centerY - dy);
-
         if (err <= 0)
         {
             dy += 1;
-            err += 2*dy + 1;
+            err += 2 * dy + 1;
         }
         if (err > 0)
         {
             dx -= 1;
-            err -= 2*dx + 1;
+            err -= 2 * dx + 1;
         }
     }
 }
-
-
-
 void Painter::createParallelogram(int size)
-{
-	for (int i = 0; i < 2; ++i) {
+{for (int i = 0; i < 2; ++i)
+    {
         moveForward(size);
         turnLeft(60);
         moveForward(size);
-        turnLeft(120);
-    }	
+        turnLeft(120); }
 }
-
-
-
 void Painter::createSquare(int size)
-{
-	for (int i = 0; i < 4; ++i) {
+{ for (int i = 0; i < 4; ++i)
+    {
         moveForward(size);
-	    turnLeft(90);
+        turnLeft(90);
     }
 }
-
-
 void Painter::moveForward(int numPixel)
 {
     int preX = x, preY = y;
     jumpForward(numPixel);
     SDL_RenderDrawLine(renderer, preX, preY, x, y);
 }
-
-
 void Painter::moveBackward(int numPixel)
 {
     moveForward(-numPixel);
 }
-
--- AdvProg_L6-Painter/painterTest.cpp
+++ AdvProg_L6-Painter/painterTest.cpp
@@ -18,7 +18,7 @@ struct TestStruct {
 };
 
 
-bool verifySetColor(SDL_Color color) {
+bool verifySetColor(SDL_Color color) { 
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Color color1 = {0, 0, 0};
@@ -43,8 +43,7 @@ bool verifyJumpForward(int numPixel, double angle, int curX, int curY, int diffX
     painter.jumpForward(numPixel);
     int diffX_ = painter.getX() - curX;
     int diffY_ = painter.getY() - curY;
-    // Alowing some slack gap
-    if (abs(diffX-diffX_) < 2 && abs(diffY-diffY_) < 2) {
+    if (diffX == diffX_ && diffY == diffY_) {
         return true;
     }
     else {
@@ -63,8 +62,7 @@ bool verifyJumpBackward(int numPixel, double angle, int curX, int curY, int diff
     painter.jumpBackward(numPixel);
     int diffX_ = painter.getX() - curX;
     int diffY_ = painter.getY() - curY;
-    // Alowing some slack gap
-    if (abs(diffX-diffX_) < 2 && abs(diffY-diffY_) < 2) {
+    if (diffX == diffX_ && diffY == diffY_) {
         return true;
     }
     else {
@@ -73,15 +71,14 @@ bool verifyJumpBackward(int numPixel, double angle, int curX, int curY, int diff
 }
 
 
-bool verifyTurnLeft(double degree, double curAngle, double expectedAngle) {
+bool verifyTurnLeft(double degree, double curAngle, double expectedAngle) { 
     SDL_Window *window;
     SDL_Renderer *renderer;
     initSDL(&window, &renderer);
     Painter painter(window, renderer);
     painter.setAngle(curAngle);
     painter.turnLeft(degree);
-    // Alowing some slack space
-    if (abs(painter.getAngle() - expectedAngle) < 1e-6) {
+    if (painter.getAngle() == expectedAngle) {
         return true;
     }
     else {
@@ -90,15 +87,14 @@ bool verifyTurnLeft(double degree, double curAngle, double expectedAngle) {
 }
 
 
-bool verifyTurnRight(double degree, double curAngle, double expectedAngle) {
+bool verifyTurnRight(double degree, double curAngle, double expectedAngle) { 
     SDL_Window *window;
     SDL_Renderer *renderer;
     initSDL(&window, &renderer);
     Painter painter(window, renderer);
     painter.setAngle(curAngle);
     painter.turnRight(degree);
-    // Alowing some slack space
-    if (abs(painter.getAngle() - expectedAngle) < 1e-6) {
+    if (painter.getAngle() == expectedAngle) {
         return true;
     }
     else {
@@ -107,7 +103,7 @@ bool verifyTurnRight(double degree, double curAngle, double expectedAngle) {
 }
 
 
-bool verifyRandomColor() {
+bool verifyRandomColor() { 
     SDL_Window *window;
     SDL_Renderer *renderer;
     initSDL(&window, &renderer);
@@ -115,7 +111,7 @@ bool verifyRandomColor() {
     painter.randomColor();
     SDL_Color color = painter.getColor();
     if (validColorValue(color.r) && validColorValue(color.g) && validColorValue(color.b)) {
-        return true;
+        return true; 
     }
     else {
         return false;
@@ -158,119 +154,119 @@ protected:
   void testMyPainterSetColor(void){
       int testSize = 4;
       std::string sharedName = "[setColor test] ";
-      TestStruct generateRandomNumberTestCases[testSize]  =
+      TestStruct generateRandomNumberTestCases[testSize]  = 
       {
         {
-            sharedName + "test normal 1",
-            verifySetColor(dict2SDL_Color(0, 255, 0)),
+            sharedName + "test normal 1", 
+            verifySetColor(dict2SDL_Color(0, 255, 0)), 
             "The color of Painter and renderer must be the same with input color."
         },
         {
-            sharedName + "test normal 2",
-            verifySetColor(dict2SDL_Color(0, 255, 255)),
+            sharedName + "test normal 2", 
+            verifySetColor(dict2SDL_Color(0, 255, 255)), 
             "The color of Painter and renderer must be the same with input color."
         },
         {
-            sharedName + "test normal 3",
-            verifySetColor(dict2SDL_Color(0, 255, 1)),
+            sharedName + "test normal 3", 
+            verifySetColor(dict2SDL_Color(0, 255, 1)), 
             "The color of Painter and renderer must be the same with input color."
         },
         {
-            sharedName + "test normal 4",
-            verifySetColor(dict2SDL_Color(100, 255, 0)),
+            sharedName + "test normal 4", 
+            verifySetColor(dict2SDL_Color(100, 255, 0)), 
             "The color of Painter and renderer must be the same with input color."
         },
       };
       runTestLoop(generateRandomNumberTestCases, testSize);
-    //
+    //   
   }
 
   void testMyPainterJumpForward(void){
       int testSize = 4;
       std::string sharedName = "[jumpForward test] ";
-      TestStruct generateRandomNumberTestCases[testSize]  =
+      TestStruct generateRandomNumberTestCases[testSize]  = 
       {
         {
-            sharedName + "test normal 1",
-            verifyJumpForward(30, 0, 0, 0, 30, 0),
+            sharedName + "test normal 1", 
+            verifyJumpForward(30, 0, 0, 0, 30, 0), 
             "Jumping to wrong postion."
         },
         {
-            sharedName + "test normal 2",
-            verifyJumpForward(30, 90, 90, 90, 0, -30),
+            sharedName + "test normal 2", 
+            verifyJumpForward(30, 90, 90, 90, 0, -30), 
             "Jumping to wrong postion."
         },
         {
-            sharedName + "test normal 3",
-            verifyJumpForward(30, 60, 90, 90, 15, -25),
+            sharedName + "test normal 3", 
+            verifyJumpForward(30, 60, 90, 90, 15, -25), 
             "Jumping to wrong postion."
         },
         {
-            sharedName + "test normal 4",
-            verifyJumpForward(50, 120, 90, 90, -24, -43),
+            sharedName + "test normal 4", 
+            verifyJumpForward(50, 120, 90, 90, -24, -43), 
             "Jumping to wrong postion."
         },
       };
       runTestLoop(generateRandomNumberTestCases, testSize);
-    //
+    //   
   }
 
   void testMyPainterJumpBackward(void){
       int testSize = 4;
       std::string sharedName = "[jumpBackward test] ";
-      TestStruct generateRandomNumberTestCases[testSize]  =
+      TestStruct generateRandomNumberTestCases[testSize]  = 
       {
         {
-            sharedName + "test normal 1",
-            verifyJumpBackward(50, 0, 90, 90, -50, 0),
+            sharedName + "test normal 1", 
+            verifyJumpBackward(50, 0, 90, 90, -50, 0), 
             "Jumping to wrong postion."
         },
         {
-            sharedName + "test normal 2",
-            verifyJumpBackward(40, 45, 90, 90, -28, 28),
+            sharedName + "test normal 2", 
+            verifyJumpBackward(40, 45, 90, 90, -28, 28), 
             "Jumping to wrong postion."
         },
         {
-            sharedName + "test normal 3",
-            verifyJumpBackward(40, 150, 90, 90, 34, 19),
+            sharedName + "test normal 3", 
+            verifyJumpBackward(40, 150, 90, 90, 34, 19), 
             "Jumping to wrong postion."
 
         },
         {
-            sharedName + "test normal 4",
-            verifyJumpBackward(15, 120, 90, 90, 7, 12),
+            sharedName + "test normal 4",  
+            verifyJumpBackward(15, 120, 90, 90, 7, 12), 
             "Jumping to wrong postion."
 
         },
       };
       runTestLoop(generateRandomNumberTestCases, testSize);
-    //
+    //   
   }
 
   void testMyPainterTurnLeft(void){
       int testSize = 4;
       std::string sharedName = "[turnLeft test] ";
-      TestStruct generateRandomNumberTestCases[testSize]  =
+      TestStruct generateRandomNumberTestCases[testSize]  = 
       {
         {
-            sharedName + "test normal 1",
-            verifyTurnLeft(30, 360, 30),
+            sharedName + "test normal 1", 
+            verifyTurnLeft(30, 360, 30), 
             "Wrong direction turn."
         },
         {
-            sharedName + "test normal 2",
-            verifyTurnLeft(30, 180, 210),
+            sharedName + "test normal 2", 
+            verifyTurnLeft(30, 180, 210), 
             "Wrong direction turn."
         },
         {
-            sharedName + "test normal 3",
-            verifyTurnLeft(-30, 120, 90),
+            sharedName + "test normal 3", 
+            verifyTurnLeft(-30, 120, 90), 
             "Wrong direction turn."
 
         },
         {
-            sharedName + "test normal 4",
-            verifyTurnLeft(0, 90, 90),
+            sharedName + "test normal 4",  
+            verifyTurnLeft(0, 90, 90), 
             "Wrong direction turn."
         },
       };
@@ -280,26 +276,26 @@ protected:
   void testMyPainterTurnRight(void){
       int testSize = 4;
       std::string sharedName = "[turnRight test] ";
-      TestStruct generateRandomNumberTestCases[testSize]  =
+      TestStruct generateRandomNumberTestCases[testSize]  = 
       {
         {
-            sharedName + "test normal 1",
-            verifyTurnRight(-30, 360, 30),
+            sharedName + "test normal 1", 
+            verifyTurnRight(-30, 360, 30), 
             "Wrong direction turn."
         },
         {
-            sharedName + "test normal 2",
-            verifyTurnRight(30, 180, 150),
+            sharedName + "test normal 2", 
+            verifyTurnRight(30, 180, 150), 
             "Wrong direction turn."
         },
         {
-            sharedName + "test normal 3",
-            verifyTurnRight(30, 0, -30),
+            sharedName + "test normal 3", 
+            verifyTurnRight(30, 0, -30), 
             "Wrong direction turn."
         },
         {
-            sharedName + "test normal 4",
-            verifyTurnRight(0, 90, 90),
+            sharedName + "test normal 4",  
+            verifyTurnRight(0, 90, 90), 
             "Wrong direction turn."
         },
       };
@@ -308,38 +304,38 @@ protected:
 
   void testMyPainterRandomColor(void){
       int testSize = 4;
-      std::string sharedName = "[randomColor test] ";
-      TestStruct generateRandomNumberTestCases[testSize]  =
+      std::string sharedName = "[turnRight test] ";
+      TestStruct generateRandomNumberTestCases[testSize]  = 
       {
         {
-            sharedName + "test normal 1",
+            sharedName + "test normal 1", 
             verifyRandomColor(),
             "The color values are not invalid."
         },
         {
-            sharedName + "test normal 2",
+            sharedName + "test normal 2", 
             verifyRandomColor(),
             "The color values are not invalid."
         },
         {
-            sharedName + "test normal 3",
-            verifyRandomColor(),
+            sharedName + "test normal 3", 
+            verifyRandomColor(), 
             "The color values are not invalid."
 
         },
         {
-            sharedName + "test normal 4",
-            verifyRandomColor(),
+            sharedName + "test normal 4",  
+            verifyRandomColor(), 
             "The color values are not invalid."
         },
       };
       runTestLoop(generateRandomNumberTestCases, testSize);
-    //
+    //   
   }
 
   void successTestExit(void) {
     std::cout << "all tests passed! \n";
-
+    
   }
 
 };
